5be454a86b0ab094d223a492bbf9b8ff3ccaffd3
vitaliy.kharlan.1977@gmail.com <vitaliy.kharlan.1977.gmail.com>
2025-01-28 23:29:15 +0200

    add BNB, add PageView

--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -1,23 +1,40 @@
-import 'package:flutter/material.dart';
-import 'package:todo_app/main.dart';
+import 'dart:developer';
+import 'package:bloc/bloc.dart';
 
-class TODOApplication extends StatelessWidget {
-  const TODOApplication({
-    super.key,
-  });
-
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    log('onCreate -- bloc: ${bloc.runtimeType}');
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'TODO application',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    log('onEvent -- bloc: ${bloc.runtimeType}, event: $event');
+  }
 
-      home: const MainScreen(),
-    );
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log('onChange -- bloc: ${bloc.runtimeType}, change: $change');
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    log('onTransition -- bloc: ${bloc.runtimeType}, transition: $transition');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    log('onError -- bloc: ${bloc.runtimeType}, error: $error');
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    log('onClose -- bloc: ${bloc.runtimeType}');
   }
 }
