84d078bb76b4a56b1f5b77bd1128433a0c8fc370
vitaliy.kharlan.1977@gmail.com <vitaliy.kharlan.1977.gmail.com>
2025-02-14 18:11:11 +0200

    add dynamic progress indicator

--- lib/features/create_new_task/view/create_new_task_screen.dart
+++ lib/features/create_new_task/view/create_new_task_screen.dart
@@ -524,11 +524,32 @@ class Task {
   final DateTime taskCreatedAt;
 
   double get progress {
-    return 60;
+    if (taskDeadline == null) return 100;
+    double progress = TaskCurrentTimeProgressIndicator.calculateProgress(
+        taskCreatedAt, taskDeadline!);
+    return progress;
   }
 
   @override
   String toString() {
     return 'Task(title: $taskTitle)';
+  }
+}
+
+class TaskCurrentTimeProgressIndicator {
+  static double calculateProgress(
+      DateTime taskCreatedAt, DateTime taskDeadline) {
+    final DateTime now = DateTime.now();
+
+    if (now.isBefore(taskCreatedAt)) return 0.0;
+    if (now.isAfter(taskDeadline)) return 100.0;
+
+    final Duration totalDuration = taskDeadline.difference(taskCreatedAt);
+    final Duration elapsedDuration = now.difference(taskCreatedAt);
+
+    double progress =
+        (elapsedDuration.inMilliseconds / totalDuration.inMilliseconds) * 100;
+
+    return double.parse(progress.toStringAsFixed(2));
   }
 }
